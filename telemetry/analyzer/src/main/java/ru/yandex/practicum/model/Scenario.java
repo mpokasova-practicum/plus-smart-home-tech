d1721f05fb7d421313937b3b0308c4c64892808a
Maria Pokasova <mpokasova99@gmail.com>
2026-02-25 23:47:43 +0300

    v1

---
+++ telemetry/analyzer/src/main/java/ru/yandex/practicum/model/Scenario.java
@@ -0,0 +1,24 @@
+package ru.yandex.practicum.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "scenarios")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Builder
+public class Scenario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "hub_id")
+    private String hubId;
+
+    private String name;
+}
